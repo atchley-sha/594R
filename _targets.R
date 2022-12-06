663be32687ace033bb7c839f2300e66fae406765
Hayden Atchley <atchley.sha@gmail.com>
2022-12-06 13:05:04 -0700

    start presentation

---
+++ _targets.R
@@ -0,0 +1,27 @@
+# Load packages required to define the pipeline:
+library(targets)
+library(tarchetypes) # Load other packages as needed. # nolint
+
+# Set target options:
+tar_option_set(
+  packages = c("tibble"), # packages that your targets need to run
+  format = "rds" # default storage format
+)
+
+
+# Run the R scripts in the R/ folder with your custom functions:
+tar_source()
+
+
+# Replace the target list below with your own:
+list(
+  tar_target(
+    name = data,
+    command = tibble(x = rnorm(100), y = rnorm(100))
+#   format = "feather" # efficient storage of large data frames # nolint
+  ),
+  tar_target(
+    name = model,
+    command = coefficients(lm(y ~ x, data = data))
+  )
+)
